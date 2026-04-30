51a50fa8ab88b20c509779c0266a4f622a9dc1c3
UPEV1sion <e.schaeffer82@gmail.com>
2026-04-30 15:22:26 +0200

    abstraction

--- heap.h
+++ heap.h
@@ -12,7 +12,45 @@
         T *items; \
         int (*cmp)(const void *, const void *); \
     }
+typedef struct {
+    size_t count;
+    size_t capacity;
+    void *items;
+    int (*cmp)(const void *, const void *);
+} Heap__Abstract;
 
+typedef struct {
+    size_t item_size;
+} Heap__Layout;
 
+#define layoutof(heap) \
+    (Heap__Layout) struct { \
+        .item_size = sizeof(*(heap)->items), \
+    }
+
+#define heap_insert(heap, item) \
+    heap__insert((Heap__Abstract *) heap, \
+            (typeof(*(heap)->items)[]{item}), \
+            layoutof(heap))
+
+#define heap_pop(heap) \
+    (typeof((heap)->items)) heap__pop((Heap__Abstract *) heap, layoutof(heap))
+
+void heap__insert(Heap__Abstract *heap, void *item, Heap__Layout *l);
+void* heap__pop(Heap__Abstract *heap, Heap__Layout *l);
 
 #endif // HEAP_H_
+
+#ifdef HEAP_IMPLEMENTATION
+
+void heap__insert(Heap__Abstract *heap, void *item, Heap__Layout *l)
+{
+
+}
+
+void* heap__pop(Heap__Abstract *heap, Heap__Layout *l)
+{
+
+}
+
+#endif // HEAP_IMPLEMENTATION
