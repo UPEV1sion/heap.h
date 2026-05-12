1fe175dfd24bd95c92d2aa77e9300b39d7f5d546
UPEV1sion <e.schaeffer82@gmail.com>
2026-05-12 09:45:47 +0200

    add heap_clear and cleanup

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Heap in C
 
-A Generic, typesafe[^1], header-only heap in C, with a nice API.
+A Generic, typesafe[^1], header-only binary heap in C, with a nice API.
+This heap implementation can serve both as a max and a min heap, and can be used as a priority queue.
 
-[^1]: with respect to the C type convertions
+[^1]: with respect to C type convertions
 
 ## Usage
 
@@ -15,7 +16,7 @@ A Generic, typesafe[^1], header-only heap in C, with a nice API.
 int main(void)
 {
     Heap(char *) heap = {
-        .cmp = heap_cmp_cstr_asc
+        .cmp = heap_cstr_asc
     };
 
     heap_insert(&heap, "lorem");
@@ -39,7 +40,18 @@ int main(void)
 ```text
 heap_insert(heap_ptr, item)   insert an item into the heap
 heap_pop(heap_ptr, out_ptr)   pop the top item of the heap
-heap_size(heap_ptr)           get the remaining heap size
+heap_size(heap_ptr)           get the number of elements in the heap
+heap_clear(heap_ptr)          remove all elements from the heap
 heap_free(heap_ptr)           free the heap memory
 ```
 
+The library also provides convenience comparators for common types:
+
+```c
+int heap_int_desc(const void *a, const void *b);
+int heap_int_asc(const void *a, const void *b);
+int heap_cstr_desc(const void *a, const void *b);
+int heap_cstr_asc(const void *a, const void *b);
+```
+
+By default `heap_int_desc` is used as a comparator
--- heap.h
+++ heap.h
@@ -42,15 +42,17 @@ typedef struct {
 
 #define heap_size(heap) (heap)->count
 
+#define heap_clear(heap) (heap)->count = 0
+
 #define heap_free(heap) free((heap)->items)
 
 void heap__insert(Heap__Abstract *heap, void *item, Heap__Layout l);
 void heap__pop(Heap__Abstract *heap, void *out, Heap__Layout l);
 
-int heap_cmp_int_desc(const void *a, const void *b);
-int heap_cmp_int_asc(const void *a, const void *b);
-int heap_cmp_cstr_desc(const void *a, const void *b);
-int heap_cmp_cstr_asc(const void *a, const void *b);
+int heap_int_desc(const void *a, const void *b);
+int heap_int_asc(const void *a, const void *b);
+int heap_cstr_desc(const void *a, const void *b);
+int heap_cstr_asc(const void *a, const void *b);
 
 #endif // HEAP_H_
 
@@ -59,28 +61,28 @@ int heap_cmp_cstr_asc(const void *a, const void *b);
 #define HEAP_INIT_CAP 1024
 #define HEAP_GROW_RATE 1.5
 
-int heap_cmp_int_desc(const void *a, const void *b)
+int heap_int_desc(const void *a, const void *b)
 {
     const int *ia = a;
     const int *ib = b;
     return (*ia > *ib) - (*ia < *ib);
 }
 
-int heap_cmp_int_asc(const void *a, const void *b)
+int heap_int_asc(const void *a, const void *b)
 {
     const int *ia = a;
     const int *ib = b;
     return (*ia < *ib) - (*ia > *ib);
 }
 
-int heap_cmp_cstr_desc(const void *a, const void *b)
+int heap_cstr_desc(const void *a, const void *b)
 {
     const char * const*ca = a;
     const char * const*cb = b;
     return strcmp(*ca, *cb);
 }
 
-int heap_cmp_cstr_asc(const void *a, const void *b)
+int heap_cstr_asc(const void *a, const void *b)
 {
     const char * const*ca = a;
     const char * const*cb = b;
@@ -122,7 +124,7 @@ void heap__heapify(Heap__Abstract *heap, size_t i, Heap__Layout l)
     const size_t left = 2 * i + 1;
     const size_t right = 2 * i + 2;
 
-    int (*cmp)(const void *, const void *) = heap->cmp ? heap->cmp : heap_cmp_int_desc;
+    int (*cmp)(const void *, const void *) = heap->cmp ? heap->cmp : heap_int_desc;
     if(left < heap->count && cmp(heap__item_at(heap, left, l), heap__item_at(heap, largest, l)) > 0)
     {
         largest = left;
@@ -145,7 +147,7 @@ void heap__insert(Heap__Abstract *heap, void *item, Heap__Layout l)
     size_t i = heap->count;
     heap__append(heap, item, l);
 
-    int (*cmp)(const void *, const void *) = heap->cmp ? heap->cmp : heap_cmp_int_desc;
+    int (*cmp)(const void *, const void *) = heap->cmp ? heap->cmp : heap_int_desc;
     while(i != 0 && cmp(heap__item_at(heap, (i - 1) / 2, l), heap__item_at(heap, i, l)) < 0)
     {
         const size_t parent = (i - 1) / 2;
@@ -169,3 +171,38 @@ void heap__pop(Heap__Abstract *heap, void *out, Heap__Layout l)
 }
 
 #endif // HEAP_IMPLEMENTATION
+
+
+/*
+    Revision history:
+
+        2.1.0 (2026-05-12) Remove cmp from the comparators for a cleaner interface
+        1.1.0 (2026-05-12) Add heap_clear
+        1.0.0 (2026-05-04) First release
+*/
+
+/*
+
+MIT License
+
+Copyright (c) 2026 Emanuel Schäffer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
